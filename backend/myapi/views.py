5af7a8580d13882429eab58ef6177ccd57cebe7b
alikaansahin <95754556+alikaansahin@users.noreply.github.com>
2024-02-24 15:36:57 +0300

    initialization

---
+++ backend/myapi/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def hello_world(request):
+        return Response({'message': 'Hello, world!'})
+
+# Create your views here.
