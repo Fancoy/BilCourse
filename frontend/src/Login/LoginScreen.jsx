948e1270f501022b38bd1c50d9ce9b4bb17a2c46
Berke Korkmaz <berkekorkmaz@yahoo.com.tr>
2024-03-03 17:21:57 +0300

    tel number max-min length fix

--- frontend/src/Login/LoginScreen.jsx
+++ frontend/src/Login/LoginScreen.jsx
@@ -22,7 +22,7 @@ export default function LoginForm() {
         <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
         <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
         <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-        <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
+        <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 10, maxLength: 10})} />
         <select {...register("Title", { required: true })}>
           <option value="Mr">Mr</option>
           <option value="Mrs">Mrs</option>
