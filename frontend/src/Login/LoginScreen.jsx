5d806df7ffee1e23cd26caed6d80f706a6d0959e
Berke Korkmaz <berkekorkmaz@yahoo.com.tr>
2024-03-05 14:50:38 +0300

    update

--- frontend/src/Login/LoginScreen.jsx
+++ frontend/src/Login/LoginScreen.jsx
@@ -19,20 +19,30 @@ export default function LoginForm() {
         {darkMode ? <DarkModeOutlinedIcon /> : <DarkModeIcon />}
       </button>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
-        <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
-        <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-        <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 10, maxLength: 10})} />
-        <select {...register("Title", { required: true })}>
+        <input type="text" placeholder="First name" {...register("firstName", { required: true, maxLength: 80 })} />
+        {errors.firstName && <span>First name is required</span>}
+
+        <input type="text" placeholder="Last name" {...register("lastName", { required: true, maxLength: 100 })} />
+        {errors.lastName && <span>Last name is required</span>}
+
+        <input type="text" placeholder="Email" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
+        {errors.email && <span>Enter a valid email address</span>}
+
+        <input type="tel" placeholder="Mobile number" {...register("mobileNumber", { required: true, minLength: 10, maxLength: 10 })} />
+        {errors.mobileNumber && <span>Enter a valid 10-digit mobile number</span>}
+
+        <select {...register("title", { required: true })}>
           <option value="Mr">Mr</option>
           <option value="Mrs">Mrs</option>
           <option value="Miss">Miss</option>
           <option value="Dr">Dr</option>
         </select>
-
-        <input {...register("Developer", { required: true })} type="radio" value="Yes" />
-        <input {...register("Developer", { required: true })} type="radio" value="No" />
-
+        {errors.title && <span>Title is required</span>}
+        <div className='radioButtonGroup'>
+          <input {...register("registrationType", { required: true })} type="radio" value="Student" name="registrationType" />Student
+          <input {...register("registrationType", { required: true })} type="radio" value="Instructor" name="registrationType" />Instructor
+        </div>
+        {errors.registrationType && <span>Please select registration type</span>}
         <input type="submit" />
       </form>
     </div>
