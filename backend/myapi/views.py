5495dd01597aced2e658578dfc8dc55ddee1515d
FaikUtku <91693278+FaikUtku@users.noreply.github.com>
2024-04-07 22:52:10 +0300

    Delete backend directory

--- backend/myapi/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-@api_view(['GET'])
-def hello_world(request):
-        return Response({'message': 'Hello, world!'})
-
-# Create your views here.
