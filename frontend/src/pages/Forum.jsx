5c124f24a1e670107a8b62ccbc3c411c7c72c5c9
FaikUtku <91693278+FaikUtku@users.noreply.github.com>
2024-05-15 01:33:55 +0300

    minor forum fix

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -9,6 +9,8 @@ function Forum() {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [newHeader, setNewHeader] = useState("");
+  const [user, setUser] = useState(null);
+  const [course, setCourse] = useState(null);
 
   useEffect(() => {
     fetchForumDetails();
@@ -18,8 +20,12 @@ function Forum() {
   const fetchForumDetails = async () => {
     try {
       const response = await api.get(`/api/forums/${forumId}`);
+      const userRes = await api.get(`/api/profile/`);
       if (response.status === 200) {
+        const courseRes = await api.get(`/api/courses/${response.data.course}`);
+        setCourse(courseRes.data);
         setForum(response.data);
+        setUser(userRes.data.user);
       } else {
         alert('Failed to fetch forum details.');
       }
@@ -67,12 +73,13 @@ function Forum() {
     <div>
         <button onClick={() => navigate(`/courses/${forum.course}/listforums`)}>Go Back</button>
       <h1>Forum: {forum.title}</h1>
-      <h2>Course ID: {forum.course}</h2>
+      <h2>Course: {course.title}</h2>
       <div>
         <h3>Messages:</h3>
         {messages.map((msg, index) => (
           <div key={index}>
-            <p>{msg.header}: {msg.content} (by {msg.sender})</p>
+            <h4> {msg.header} </h4>
+            <p> {msg.content} (by {user.email})</p>
           </div>
         ))}
         <div>
